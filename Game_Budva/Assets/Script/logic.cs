058a9b159e795bc4b78b1b5d2ea80d7f1849c5b8
Nikolay1166 <rom.nik66@gmail.com>
2024-12-02 11:28:04 +0100

    1

---
+++ Game_Budva/Assets/Script/logic.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Logic : MonoBehaviour
+{
+    public int[] keys = new int[3];
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
