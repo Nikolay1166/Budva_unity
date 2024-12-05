b11a30694764b18ec6959b2287ede35ccafdc716
novin36 <gljebleskiv@gmail.com>
2024-12-05 16:32:38 +0100

    stena

--- Game_Budva/Assets/Script/Logic.cs
+++ Game_Budva/Assets/Script/Logic.cs
@@ -9,12 +9,26 @@ using UnityEngine.SceneManagement;
 public class Logic : MonoBehaviour
 {
 
-public  int [] Keys = new int[3];
+public int [] Keys = new int[3];
 
+   
+
+
+
+
+   
     private void OnTriggerEnter2D(Collider2D other)
 {
        
-    
+    if (other.gameObject.tag == "Key0" ){
+        Keys[0] = 1;
+        Destroy (other.gameObject);
+    }     
+        
+    if (other.gameObject.tag == "Key1" & Keys[0] == 1 ){
+        Keys[1] = 1;
+        Destroy (other.gameObject);
+    }   
     if (other.gameObject.tag == "Key2" & Keys[1] == 1 ){
        SceneManager.LoadScene (2);
     }      
