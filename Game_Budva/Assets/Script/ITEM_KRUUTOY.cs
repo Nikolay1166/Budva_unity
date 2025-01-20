d39a0fb8f79b97ea2b07f3d40690720107f4d625
novin36 <gljebleskiv@gmail.com>
2025-01-20 16:51:13 +0100

    JA KRUTOJ
    NE VRUUUUUUUUUUUU

---
+++ Game_Budva/Assets/Script/ITEM_KRUUTOY.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "ItemData",menuName = "Nicho/Item")]
+public class ITEM_KRUUTOY : ScriptableObject
+{
+ public string name = "Item";
+ public int price = 500;
+ public int waight = 1;
+ public int strenght = 60; 
+ public int MaxObjects = 10;
+
+}
